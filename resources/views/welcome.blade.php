cd35367283fb031d5ec10bc91c320523d37b956c
Patrick Hsieh <patrick_hsieh@owlting.com>
2019-03-25 14:27:04 +0800

    update welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 in Owlting</div>
+                <div class="title">Laravel 5 in Owlting in Taipei</div>
             </div>
         </div>
     </body>
